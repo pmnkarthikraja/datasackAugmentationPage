4c39adc9f005315b1f8b210afab66c2f1fefcf09
karthikraja <pmnkarthikraja@gmail.com>
2024-07-15 13:15:38 +0530

    add actual mobile number with welcome template

--- src/components/FabButton.tsx
+++ src/components/FabButton.tsx
@@ -2,9 +2,35 @@ import { FunctionComponent } from 'react'
 import styles from '../styles/FabButton.module.css'
 
 const FabButton:FunctionComponent = () =>{
-    return    <a
+  const phoneNumber = +966560858596
+  const message = encodeURIComponent(
+    `
+    Hi there, Explore IT Staff Augmentation Services in Riyadh
+
+    Need Tech Talent Quickly?
+
+    Discover the advantages of our IT Staff Augmentation Services:
+
+    - Strategic Hiring Efficiency: Reduce costs by hiring as needed.
+    - Flexible Workforce: Scale your team as demand fluctuates.
+    - Precision Staffing: Optimize resources with just-in-time hiring.
+    - Rapid Expansion: Grow efficiently to meet market demands.
+    - Core Focus: Outsource non-core tasks for better focus.
+    - Onsite Efficiency: Ensure prompt support with streamlined logistics.
+    - Smooth Visa Process: Expert handling for hassle-free operations.
+    - Resource Deployment: Agile deployment to match business needs.
+
+      Contact us to learn more (https://www.datasack.in)! 
+    `
+  );
+
+
+  const whatsappUrl = `https://wa.me/${phoneNumber}?text=${message}`;
+
+
+  return    <a
     className={styles.fabButton}
-    href="https://wa.me/+911234567788" 
+    href={whatsappUrl}
     target="_blank"
     rel="noopener noreferrer"
   >
